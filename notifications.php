bcf4f21e0bdbe25005fb7780573344a3d11cc86b
Uriel Sosa <sosa.uriel1999@gmail.com>
2020-10-30 00:21:42 -0300

    fix notifications

--- notifications.php
+++ notifications.php
@@ -1,16 +1,21 @@
-<?php 
-    require_once 'controllers/MercadoPagoController.php';
+<?php
+    require_once __DIR__ . '/vendor/autoload.php';
+
 
     if($_SERVER['REQUEST_METHOD'] === "POST"){
+        $access_token = "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
+        MercadoPago\SDK::setAccessToken($access_token);
         
         $json = file_get_contents("php://input");
-        $body = json_decode($json);
         
         header('Content-Type: application/json');
-
+        
         error_log("========= Request Body ========= $json", 0);	
         
         echo $json;
-        echo $body;
+  
+        $body = json_decode($json);
+  
+        http_response_code(200);    
     }
 ?>
